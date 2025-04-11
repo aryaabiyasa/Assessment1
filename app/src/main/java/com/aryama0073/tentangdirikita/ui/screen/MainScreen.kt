f05e562108b84bad337af402160c2d2327c86685
Arya Meidi Abiyasa <abiyasa1331@gmail.com>
2025-04-11 17:16:07 +0700

    Menambahkan Pilihan Gender

--- app/src/main/java/com/aryama0073/tentangdirikita/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aryama0073/tentangdirikita/ui/screen/MainScreen.kt
@@ -1,18 +1,21 @@
 package com.aryama0073.tentangdirikita.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +23,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -35,7 +39,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -74,6 +80,11 @@ fun ScreenContent(modifier: Modifier = Modifier) {
     var umur by rememberSaveable { mutableStateOf("") }
     var umurerror by rememberSaveable { mutableStateOf(false) }
 
+    val radioOptions = listOf(
+        stringResource(id = R.string.pria),
+        stringResource(id = R.string.wanita)
+    )
+    var gender by rememberSaveable { mutableStateOf(radioOptions[0]) }
     var isDatepickerOpen by remember  { mutableStateOf(false) }
 
     var tanggallahir by rememberSaveable { mutableStateOf("") }
@@ -152,6 +163,40 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 }
             },
             modifier = Modifier.fillMaxWidth()
+        )
+        Row (
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ) {
+            radioOptions.forEach { text ->
+                GenderOption(
+                    label = text,
+                    isSelected = gender == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = gender == text,
+                            onClick = { gender = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+@Composable
+fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row (
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
         )
     }
 }
