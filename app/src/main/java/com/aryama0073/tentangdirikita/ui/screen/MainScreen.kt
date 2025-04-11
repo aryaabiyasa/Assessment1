165d194a1fa18f51b13ae520fd56039193796066
Arya Meidi Abiyasa <abiyasa1331@gmail.com>
2025-04-11 17:04:28 +0700

    Menambahkan Input Tanggal Lahir

--- app/src/main/java/com/aryama0073/tentangdirikita/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aryama0073/tentangdirikita/ui/screen/MainScreen.kt
@@ -10,18 +10,27 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -33,6 +42,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aryama0073.tentangdirikita.R
 import com.aryama0073.tentangdirikita.ui.theme.TentangDiriKitaTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -61,6 +73,26 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 
     var umur by rememberSaveable { mutableStateOf("") }
     var umurerror by rememberSaveable { mutableStateOf(false) }
+
+    var isDatepickerOpen by remember  { mutableStateOf(false) }
+
+    var tanggallahir by rememberSaveable { mutableStateOf("") }
+
+    var date by remember { mutableLongStateOf(0) }
+
+    if (isDatepickerOpen) {
+        DatePickerModal(
+            onDateSelected = {
+                if (it != null) {
+                    date = it
+                    val formatter = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+                    tanggallahir = formatter.format(Date(it))
+                }
+            }
+        ) {
+            isDatepickerOpen = false
+        }
+    }
 
     Column (
     modifier = modifier.fillMaxSize()
@@ -102,6 +134,25 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        OutlinedTextField(
+            value = tanggallahir,
+            onValueChange = {},
+            label = { Text(text = stringResource(R.string.tanggal_lahir)) },
+            readOnly = true,
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        isDatepickerOpen = true
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.DateRange,
+                        contentDescription = "Pilih Tanggal"
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
@@ -118,6 +169,34 @@ fun IconPicker(isError: Boolean, unit: String) {
 fun ErrorHint(isError: Boolean) {
     if (isError) {
         Text(text = stringResource(R.string.input_invalid))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
     }
 }
 
