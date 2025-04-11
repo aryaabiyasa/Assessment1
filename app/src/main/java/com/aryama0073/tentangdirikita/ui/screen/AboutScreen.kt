775c2af09f0b152228e6cbf66e568b45067f1957
Arya Meidi Abiyasa <abiyasa1331@gmail.com>
2025-04-11 21:41:55 +0700

    Menambah image di About

--- app/src/main/java/com/aryama0073/tentangdirikita/ui/screen/AboutScreen.kt
+++ app/src/main/java/com/aryama0073/tentangdirikita/ui/screen/AboutScreen.kt
@@ -1,6 +1,10 @@
 package com.aryama0073.tentangdirikita.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -9,11 +13,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +36,8 @@ import com.aryama0073.tentangdirikita.ui.theme.TentangDiriKitaTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(navController: NavHostController) {
+    var isEnglish by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -40,6 +53,23 @@ fun AboutScreen(navController: NavHostController) {
                 title = {
                     Text(text = stringResource(id = R.string.tentang_aplikasi))
                 },
+                actions = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically, // Align vertically
+                        horizontalArrangement = Arrangement.Center // Center the content horizontally
+                    ) {
+                        // Switch for language selection
+                        Text(
+                            text = if (isEnglish) "ENG" else "IDN",
+                            modifier = Modifier.padding(end = 8.dp),
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Switch(
+                            checked = isEnglish,
+                            onCheckedChange = { isEnglish = it },
+                        )
+                    }
+                },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
@@ -47,10 +77,28 @@ fun AboutScreen(navController: NavHostController) {
             )
         }
     ) { innerPadding ->
-        Text(
-            text = stringResource(R.string.copyright),
-            modifier = Modifier.padding(innerPadding).padding(16.dp)
-        )
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp), // Space out text and image
+            horizontalAlignment = Alignment.CenterHorizontally // Center the content horizontally
+        ) {
+            Text(
+                text = if (isEnglish) {
+                    "A simple application that can create a short biodata about yourself by filling in your name, age, date of birth, choosing gender, hobbies, and choosing your favorite animal."
+                } else {
+                    "Aplikasi sederhana yang dapat membuatkan anda biodata singkat tentang diri anda dari mengisikan nama, mengisi umur, tanggal lahir, memilih jenis kelamin, hobi, dan memilih hewan favorit kita"
+                },
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.image ),
+                contentDescription = "Calculator image",
+                modifier = Modifier.padding(top = 5.dp)
+            )
+        }
     }
 }
 
